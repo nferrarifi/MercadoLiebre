12cb3cb7cb317ebaf498c2652237b5ea1c23f2e3
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-03-24 19:08:51 -0300

    Mercadoliebre al dia del 24/3

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const path = require ("path")
 app.use(express.static("public"))
 
 app.listen (3000, () => {
-    console.log ("servidor corriendo en el puerto 3000 aaaaaaaa")
+    console.log ("servidor corriendo en el puerto 3000")
 
 })
+
+
 
 app.get ("/", (req,res) => {
     res.sendFile(path.resolve("./views/index.html"))
 })
 
+app.get ("/register", (req,res) => {
+    res.sendFile(path.resolve("./views/register.html"))
+})
+
+app.get ("/login", (req,res) => {
+    res.sendFile(path.resolve("./views/login.html"))
+})
