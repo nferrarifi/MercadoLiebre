95ce1d4eb6de52b9c6b5e8997fb60d9b590a1178
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-03-24 20:13:10 -0300

    test

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require ("express")
 const app = express()
 const path = require ("path")
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static("public"))
 
 app.listen (3000, () => {
